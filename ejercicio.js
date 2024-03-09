383199eccdd06c72170f4a14b4ec9e06eb0dc779
XaviEduardo <xavi_ed@outlook.com>
2024-03-08 22:57:28 -0700

    update

--- ejercicio.js
+++ ejercicio.js
@@ -1,13 +1,16 @@
 function cambiarColor()
 {
     variable_album = document.getElementById("album");
-    variable_album.style.backgroundColor = "gainsboro";
-
-
+    if (variable_album.style.backgroundColor === "transparent" || variable_album.style.backgroundColor === "") {
+        variable_album.style.backgroundColor = "gainsboro"; // Cambia al color de fondo establecido
+    } else {
+        variable_album.style.backgroundColor = "transparent"; // Hace el fondo transparente
+    }
 }
 
 function cambiarTamano(){
-    clase_imagen = document.getElementsByClassName("imagen")[0];
-    clase_imagen.style.height = "350";
+    let clase_imagen = document.getElementsByClassName("imagen")[0];
+    clase_imagen.style.height="250px";
     clase_imagen.style.width="auto";
+
 }
