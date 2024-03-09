43bead6b9d030798b8285aef7d8508e5874025a6
XaviEduardo <xavi_ed@outlook.com>
2024-03-08 21:03:51 -0700

    update

---
+++ ejercicio.js
@@ -0,0 +1,13 @@
+function cambiarColor()
+{
+    variable_album = document.getElementById("album");
+    variable_album.style.backgroundColor = "green";
+
+
+}
+
+function cambiarTamano(){
+    clase_imagen = document.getElementsByClassName("imagen");
+    
+
+}
